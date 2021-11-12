fa0429c8433ddf3465a0fa452a3f0bcae1cc2747
strangerweather <strangerweather@gmail.com>
2021-11-12 20:17:11 +0000

    implemented zac lib

--- app/src/main/java/com/strangerweather/rootlessuimods/MainActivity.kt
+++ app/src/main/java/com/strangerweather/rootlessuimods/MainActivity.kt
@@ -1,27 +1,26 @@
 package com.strangerweather.rootlessuimods
 
-import android.annotation.SuppressLint
 import android.content.pm.PackageManager
 import android.os.Bundle
-import android.os.IBinder
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.AlertDialog
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.strangerweather.rootlessuimods.ui.theme.RootlessUIModsTheme
 import org.lsposed.hiddenapibypass.HiddenApiBypass
 import rikka.shizuku.Shizuku
 import rikka.shizuku.Shizuku.checkSelfPermission
-import rikka.shizuku.ShizukuBinderWrapper
 import rikka.shizuku.ShizukuProvider
-import rikka.shizuku.SystemServiceHelper.getSystemService
 import kotlin.streams.toList
 
 
@@ -42,8 +41,7 @@ class MainActivity : ComponentActivity() {
                 Surface(color = MaterialTheme.colors.background) {
                     if (!shizukuAvailable) ShowShizukuDialog() else checkShizukuPermission()
                     if (checkShizukuPermission()) {
-                        accessApis()
-//                        AdbTest()
+                        AdbTest()
                     }
                 }
             }
@@ -70,43 +68,14 @@ fun checkShizukuPermission(): Boolean {
     return isGranted
 }
 
-@SuppressLint("PrivateApi")
-fun accessApis() {
-    val iPmClass = Class.forName("android.content.om.IOverlayManager")
-    val iPmStub = Class.forName("android.content.om.IOverlayManager\$Stub")
-    val asInterfaceMethod = iPmStub.getMethod("asInterface", IBinder::class.java)
-    val grantRuntimePermissionMethod = iPmClass.getMethod(
-        "grantRuntimePermission",
-        String::class.java /* package name */,
-        String::class.java /* permission name */,
-        Int::class.java /* user ID */
-    )
-
-    val iPmInstance = asInterfaceMethod.invoke(
-        null,
-        ShizukuBinderWrapper(getSystemService("overlay"))
-    )
-
-    grantRuntimePermissionMethod.invoke(
-        iPmInstance, "com.strangerweather.rootlessuimods",
-        android.Manifest.permission.WRITE_SECURE_SETTINGS, 0
-    )
-}
-
 @Composable
 fun AdbTest() {
+    val context = LocalContext.current
 
+    val installedAppsList = context.packageManager.getInstalledApplications(0).toList()
 
-    val command = "cmd overlay list"
-
-//    val command = "cmd overlay fabricate com.android.systemui:integer/quick_settings_num_columns 3"
-//
-    ProcessBuilder().command("sh", "-c", command).start().run {
-        val lines = inputStream.bufferedReader().lines().toList()
-        destroy()
-
-        Text(text = lines.toString())
-    }
+    LazyColumn{item { Text(text = installedAppsList.toString()) }}
 }
+
 
 
