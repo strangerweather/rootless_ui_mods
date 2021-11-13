a401c27b15728f3e7d07e2e1630b353689a4149d
strangerweather <strangerweather@gmail.com>
2021-11-13 20:23:11 +0000

    added save function

--- app/src/main/java/com/strangerweather/rootlessuimods/MainActivity.kt
+++ app/src/main/java/com/strangerweather/rootlessuimods/MainActivity.kt
@@ -1,81 +1,87 @@
 package com.strangerweather.rootlessuimods
 
-import android.content.pm.PackageManager
+import android.content.Context
+import android.content.pm.ApplicationInfo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.AlertDialog
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.strangerweather.rootlessuimods.ui.theme.RootlessUIModsTheme
 import org.lsposed.hiddenapibypass.HiddenApiBypass
-import rikka.shizuku.Shizuku
-import rikka.shizuku.Shizuku.checkSelfPermission
-import rikka.shizuku.ShizukuProvider
-import kotlin.streams.toList
+import tk.zwander.fabricateoverlay.FabricatedOverlay
+import tk.zwander.fabricateoverlay.FabricatedOverlayEntry
+import tk.zwander.fabricateoverlay.OverlayAPI
+import tk.zwander.fabricateoverlay.ShizukuUtils
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         HiddenApiBypass.setHiddenApiExemptions("L")
-
-        val shizukuAvailable = Shizuku.pingBinder()
-        println("shizukuAvailable = $shizukuAvailable")
-
-        ShizukuProvider.enableMultiProcessSupport(true)
 
         setContent {
             RootlessUIModsTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    if (!shizukuAvailable) ShowShizukuDialog() else checkShizukuPermission()
-                    if (checkShizukuPermission()) {
-                        AdbTest()
+                    val overlayEntries = remember { mutableStateListOf<FabricatedOverlayEntry>() }
+                    val context = LocalContext.current
+
+                    if (!ShizukuUtils.shizukuAvailable) {
+                        ShowShizukuDialog()
+                    }
+
+                    //TODO: add permission dialog
+                    if (ShizukuUtils.hasShizukuPermission(this)) {
+//                        SaveOverlay(
+//                            context = context,
+//                            info = applicationInfo,
+//                            overlayEntries = overlayEntries
+//                        )
                     }
                 }
             }
         }
     }
-}
 
-@Composable
-fun ShowShizukuDialog() {
-    AlertDialog(modifier = Modifier.width(200.dp),
-        title = { Text(stringResource(id = R.string.shizuku_needed_title)) },
-        text = { Text(stringResource(id = R.string.shizuku_needed_text)) },
-        buttons = {},
-        onDismissRequest = {})
-}
-
-
-fun checkShizukuPermission(): Boolean {
-    val isGranted = if (Shizuku.isPreV11() || Shizuku.getVersion() < 11) {
-        checkSelfPermission() == PackageManager.PERMISSION_GRANTED
-    } else {
-        checkSelfPermission() == PackageManager.PERMISSION_GRANTED
+    @Composable
+    fun ShowShizukuDialog() {
+        AlertDialog(modifier = Modifier.width(200.dp),
+            title = { Text(stringResource(id = R.string.shizuku_needed_title)) },
+            text = { Text(stringResource(id = R.string.shizuku_needed_text)) },
+            buttons = {},
+            onDismissRequest = {})
     }
-    return isGranted
 }
 
 @Composable
-fun AdbTest() {
-    val context = LocalContext.current
+fun SaveOverlay(
+    context: Context,
+    info: ApplicationInfo,
+    overlayEntries: List<FabricatedOverlayEntry>
+) {
+    val name by remember { mutableStateOf("") }
 
-    val installedAppsList = context.packageManager.getInstalledApplications(0).toList()
-
-    LazyColumn{item { Text(text = installedAppsList.toString()) }}
+    TextButton(
+        onClick = {
+            OverlayAPI.getInstance(context) { api ->
+                api.registerFabricatedOverlay(
+                    FabricatedOverlay(
+                        "${context.packageName}.${info.packageName}.${name}",
+                        info.packageName
+                    ).apply {
+                        overlayEntries.forEach { overlay ->
+                            entries[overlay.resourceName] = overlay
+                        }
+                    }
+                )
+            }
+        }) { Text("Save") }
 }
 
-
-
+//adb shell sh /storage/emulated/0/Android/data/moe.shizuku.privileged.api/start.sh
