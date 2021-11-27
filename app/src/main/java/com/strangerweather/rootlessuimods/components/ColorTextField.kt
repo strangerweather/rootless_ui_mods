caa2497ce7a84b3e781342fa0a5d3bc47634df4a
strangerweather <strangerweather@gmail.com>
2021-11-27 15:36:10 +0000

    color of textfield

--- app/src/main/java/com/strangerweather/rootlessuimods/components/ColorTextField.kt
+++ app/src/main/java/com/strangerweather/rootlessuimods/components/ColorTextField.kt
@@ -2,15 +2,14 @@ package com.strangerweather.rootlessuimods.components
 
 import android.content.Context
 import android.widget.Toast
+import androidx.compose.foundation.background
+import androidx.compose.foundation.focusable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Palette
@@ -20,9 +19,11 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.strangerweather.rootlessuimods.R
+import com.strangerweather.rootlessuimods.ui.theme.Purple500
 import com.strangerweather.rootlessuimods.utils.MainViewModel
 
 
@@ -61,11 +62,17 @@ fun ColorTextField(context: Context, name: String, target: String, overlay: Stri
                 hex = colorCode
             }
         },
-            label = { Text(text = "Enter Hex Value") }, singleLine = true,
+            label = { Text(text = "Enter Hex Value") },
+            singleLine = true,
+            colors = TextFieldDefaults.outlinedTextFieldColors(
+                focusedBorderColor = Purple500,
+                unfocusedBorderColor = Purple500
+            ),
             leadingIcon = {
                 Icon(
                     imageVector = Icons.Filled.Palette,
-                    contentDescription = "palette icon"
+                    contentDescription = "palette icon",
+                    tint = Purple500
                 )
             },
             trailingIcon = {
@@ -74,11 +81,11 @@ fun ColorTextField(context: Context, name: String, target: String, overlay: Stri
                 }) {
                     Icon(
                         imageVector = Icons.Filled.Check,
-                        contentDescription = "check icon"
+                        contentDescription = "check icon",
+                        tint = Purple500
                     )
                 }
             },
-
             keyboardOptions = KeyboardOptions(
                 KeyboardCapitalization.Characters
             ),
