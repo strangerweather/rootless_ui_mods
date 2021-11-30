a3842907aa8ba6783dcc69a64a45885c8fb933d7
strangerweather <strangerweather@gmail.com>
2021-11-30 14:12:04 +0000

    home screen presentation 1

--- app/src/main/java/com/strangerweather/rootlessuimods/content/HomeScreenContent.kt
+++ app/src/main/java/com/strangerweather/rootlessuimods/content/HomeScreenContent.kt
@@ -1,8 +1,36 @@
 package com.strangerweather.rootlessuimods.content
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.strangerweather.rootlessuimods.R
 
 @Composable
-fun HomeScreen(){
+fun HomeScreen() {
+    Column(
+        Modifier
+            .fillMaxSize()
+            .padding(20.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            modifier = Modifier.padding(20.dp),
+            text = stringResource(id = R.string.home_screen_title),
+            fontWeight = FontWeight.Bold
+        )
+        Text(text = stringResource(id = R.string.home_screen_text1))
+
+    }
+
 
 }
