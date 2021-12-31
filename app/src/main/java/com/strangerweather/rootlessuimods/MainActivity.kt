4861cba2e76517b80395f0911fb1c42337e67b5b
strangerweather <strangerweather@gmail.com>
2021-12-31 10:22:20 +0000

    added optin

--- app/src/main/java/com/strangerweather/rootlessuimods/MainActivity.kt
+++ app/src/main/java/com/strangerweather/rootlessuimods/MainActivity.kt
@@ -1,18 +1,15 @@
 package com.strangerweather.rootlessuimods
 
-import android.content.Context
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.material.*
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.graphics.ExperimentalGraphicsApi
 import androidx.compose.ui.res.stringResource
-import androidx.datastore.preferences.core.booleanPreferencesKey
-import androidx.datastore.preferences.core.edit
-import androidx.datastore.preferences.preferencesDataStore
 import androidx.navigation.NavDestination
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavGraph.Companion.findStartDestination
@@ -25,19 +22,14 @@ import com.strangerweather.rootlessuimods.navigation.BottomBarScreen
 import com.strangerweather.rootlessuimods.navigation.BottomNavGraph
 import com.strangerweather.rootlessuimods.ui.theme.RootlessUIModsTheme
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import org.lsposed.hiddenapibypass.HiddenApiBypass
 import tk.zwander.fabricateoverlay.ShizukuUtils
 
-
+@ExperimentalPagerApi
 @ExperimentalGraphicsApi
 class MainActivity : ComponentActivity() {
-
-
-
-    @ExperimentalPagerApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         HiddenApiBypass.setHiddenApiExemptions("L")
